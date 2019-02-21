d436460311a6ebdc85d77bf8de9a184fb1a1233d
FeigelJanos <32643682+FeigelJanos@users.noreply.github.com>
2019-02-21 19:57:42 +0100

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,11 @@
 
+function palindrome(str) {
+  //Create an empty array
+  //Separate the input string into characters
+  //Fil the array with the characters
+  //Filter out non letter or number characters into a new array
+  //Check if the first and last letters are the same
+  //Check the next pair... until the last pair or the middle character if the array has an odd number of elements
+  //If the checked characters do not match return false
+  return true;
+}
